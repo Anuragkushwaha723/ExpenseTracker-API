15ef999c564a055215c3609a45e4e0c3cdd11dd4
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-04-15 22:18:47 +0530

    Creating routes and controlers of login page

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,13 @@
 const User = require('../models/user');
 
-module.exports.postUserDetails = async (req, res, next) => {
+module.exports.postUserSignUp = async (req, res, next) => {
     try {
         const name = req.body.name;
         const email = req.body.email;
         const password = req.body.password;
 
         if (!name || !email || !password) {
-            throw new Error('Missing some data');
+            res.status(500).json({ error: 'Missing some data' });
         }
 
         let responseData = await User.create({
@@ -18,5 +18,29 @@ module.exports.postUserDetails = async (req, res, next) => {
         res.status(201).json(responseData);
     } catch (error) {
         res.status(500).json({ error: error });
+    }
+};
+
+module.exports.postUserLogin = async (req, res, next) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        if (!email || !password) {
+            res.status(500).json({ message: 'Missing some data' });
+        }
+
+        let responseData = await User.findAll({ where: { email: email } });
+        if (responseData.length > 0) {
+            if (responseData[0].password === password) {
+                res.status(201).json({ message: 'User logged in successfully' });
+            } else {
+                res.status(500).json({ message: 'Password is incorrect' });
+            }
+
+        } else {
+            res.status(500).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error });
     }
 };
--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,6 @@ const express = require('express');
 const userController = require('../controllers/user');
 const route = express.Router();
 
-route.post('/signUp', userController.postUserDetails);
+route.post('/signUp', userController.postUserSignUp);
+route.post('/login', userController.postUserLogin);
 module.exports = route;
