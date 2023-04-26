aa58040aa2570a1e5188eb98c7035ea7ae969f4c
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-04-26 23:08:05 +0530

    Connecting our request with transaction rollback, commit

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,6 +1,7 @@
 const Expense = require('../models/expense');
-
+const sequelize = require('../utils/database');
 exports.postExpenseData = async (req, res, next) => {
+    let t = await sequelize.transaction();
     try {
         const amount = req.body.amount;
         const description = req.body.description;
@@ -9,12 +10,14 @@ exports.postExpenseData = async (req, res, next) => {
         if (!amount || !description || !category) {
             return res.status(500).json({ message: 'Missing some data' });
         }
-        let data = await req.user.createExpense({ amount: amount, description: description, category: category });
-        let totalExpense = req.user.totalExpense + amount;
-        await req.user.update({ totalExpense: totalExpense });
+        let data = await req.user.createExpense({ amount: amount, description: description, category: category }, { transaction: t });
+        let totalExpense = Number(req.user.totalExpense) + Number(amount);
+        await req.user.update({ totalExpense: totalExpense }, { transaction: t });
+        await t.commit();
         return res.status(201).json(data);
     } catch (error) {
-        res.status(500).json({ message: error });
+        await t.rollback();
+        return res.status(500).json({ message: error });
     }
 };
 
@@ -28,11 +31,23 @@ exports.getExpenseData = async (req, res, next) => {
 };
 
 exports.deleteExpenseData = async (req, res, next) => {
+    let t = await sequelize.transaction();
     try {
         const prodId = req.params.id;
-        await Expense.destroy({ where: { id: prodId, userId: req.user.id } });
+        let data = await Expense.findAll({ where: { id: prodId, userId: req.user.id } }, { transaction: t });
+        let updateExpense;
+        let newExpense = Number(req.user.totalExpense) - Number(data[0].amount);
+        if (newExpense > 0) {
+            updateExpense = newExpense;
+        } else {
+            updateExpense = 0;
+        }
+        await req.user.update({ totalExpense: updateExpense }, { transaction: t });
+        await data[0].destroy({ transaction: t });
+        await t.commit();
         res.status(201).json(prodId);
     } catch (error) {
+        await t.rollback();
         res.status(500).json({ message: error });
     }
 };
